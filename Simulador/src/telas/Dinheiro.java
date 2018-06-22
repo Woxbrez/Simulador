5df01b9485c3d167fab285429d79c17ee3cd4ec1
Daniel Soares <danielsoaresf@hotmail.com>
2018-06-22 05:25:45 -0300

    Tela Dinheiro

--- Simulador/src/telas/Dinheiro.java
+++ Simulador/src/telas/Dinheiro.java
@@ -1,35 +1,35 @@
 package telas;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JTextField;
 import java.awt.Font;
-import java.awt.Color;
 import java.awt.SystemColor;
-import javax.swing.JTextPane;
-import javax.swing.JRadioButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Principal extends JFrame {
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
+public class Dinheiro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField tfValorCliente;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Principal frame = new Principal();
+					Dinheiro frame = new Dinheiro();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -41,7 +41,7 @@ public class Principal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Principal() {
+	public Dinheiro() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 450);
 		contentPane = new JPanel();
@@ -65,12 +65,12 @@ public class Principal extends JFrame {
 		pResultado.add(lblNewLabel);
 		
 		JPanel pValorVenda = new JPanel();
-		pValorVenda.setBounds(10, 35, 343, 64);
+		pValorVenda.setBounds(10, 63, 343, 64);
 		pResultado.add(pValorVenda);
 		pValorVenda.setLayout(null);
 		
-		JLabel lblValorDeVenda = new JLabel("VALOR DE VENDA:");
-		lblValorDeVenda.setBounds(136, 11, 105, 14);
+		JLabel lblValorDeVenda = new JLabel("VALOR:");
+		lblValorDeVenda.setBounds(147, 11, 105, 14);
 		pValorVenda.add(lblValorDeVenda);
 		
 		JLabel lblR_2 = new JLabel("R$:");
@@ -79,7 +79,7 @@ public class Principal extends JFrame {
 		
 		JPanel pCustoFinal = new JPanel();
 		pCustoFinal.setLayout(null);
-		pCustoFinal.setBounds(10, 110, 343, 72);
+		pCustoFinal.setBounds(10, 152, 343, 72);
 		pResultado.add(pCustoFinal);
 		
 		JLabel lblCustoFinal = new JLabel("CUSTO FINAL:");
@@ -90,56 +90,9 @@ public class Principal extends JFrame {
 		label.setBounds(127, 20, 114, 14);
 		pCustoFinal.add(label);
 		
-		JLabel lblRefere = new JLabel("referente a taxa de parcelamento comprador de 2,99% ao m\u00EAs");
-		lblRefere.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		lblRefere.setBounds(50, 57, 267, 15);
-		pCustoFinal.add(lblRefere);
-		
 		JLabel lblXParcelasDe = new JLabel("X parcelas de R$ X");
 		lblXParcelasDe.setBounds(105, 40, 174, 14);
 		pCustoFinal.add(lblXParcelasDe);
-		
-		JPanel pRecebe = new JPanel();
-		pRecebe.setLayout(null);
-		pRecebe.setBounds(10, 193, 343, 64);
-		pResultado.add(pRecebe);
-		
-		JLabel lblValorDeVenda_1 = new JLabel("VALOR DE VENDA:");
-		lblValorDeVenda_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblValorDeVenda_1.setBounds(10, 11, 96, 14);
-		pRecebe.add(lblValorDeVenda_1);
-		
-		JLabel lblValorDescontado = new JLabel("VALOR DESCONTADO:");
-		lblValorDescontado.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblValorDescontado.setBounds(116, 11, 119, 14);
-		pRecebe.add(lblValorDescontado);
-		
-		JLabel lblVocRecebe = new JLabel("VOC\u00CA RECEBE:");
-		lblVocRecebe.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblVocRecebe.setBounds(255, 11, 88, 14);
-		pRecebe.add(lblVocRecebe);
-		
-		JLabel lblRX = new JLabel("R$: X");
-		lblRX.setBounds(20, 36, 46, 14);
-		pRecebe.add(lblRX);
-		
-		JLabel label_1 = new JLabel("R$: X");
-		label_1.setBounds(145, 36, 46, 14);
-		pRecebe.add(label_1);
-		
-		JLabel label_2 = new JLabel("R$: X");
-		label_2.setBounds(265, 36, 46, 14);
-		pRecebe.add(label_2);
-		
-		JLabel label_3 = new JLabel("-");
-		label_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		label_3.setBounds(100, 36, 16, 14);
-		pRecebe.add(label_3);
-		
-		JLabel label_4 = new JLabel("=");
-		label_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		label_4.setBounds(228, 36, 16, 14);
-		pRecebe.add(label_4);
 		
 		JPanel pLucro = new JPanel();
 		pLucro.setLayout(null);
@@ -156,47 +109,31 @@ public class Principal extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblComprador = new JLabel("Comprador");
-		lblComprador.setBounds(15, 215, 80, 14);
-		panel.add(lblComprador);
-		
 		tfValorCliente = new JTextField();
 		tfValorCliente.setBounds(35, 75, 86, 20);
 		panel.add(tfValorCliente);
 		tfValorCliente.setColumns(10);
 		
-		JLabel lblR_1 = new JLabel("R$");
-		lblR_1.setBounds(15, 125, 80, 14);
-		panel.add(lblR_1);
-		
 		JLabel lblCompradorPagaEm = new JLabel("Comprador paga em:");
-		lblCompradorPagaEm.setBounds(15, 150, 126, 14);
+		lblCompradorPagaEm.setBounds(15, 105, 126, 14);
 		panel.add(lblCompradorPagaEm);
 		
 		JComboBox cbParcela = new JComboBox();
-		cbParcela.setBounds(15, 170, 80, 20);
+		cbParcela.setBounds(15, 122, 80, 20);
 		panel.add(cbParcela);
 		cbParcela.setMaximumRowCount(12);
 		cbParcela.setModel(new DefaultComboBoxModel(new String[] {"1 parcela", "2 parcelas", "3 parcelas", "4 parcelas", "5 parcelas", "6 parcelas", "7 parcelas", "8 parcelas", "9 parcelas", "10 parcelas", "11 parcelas", "12 parcelas"}));
 		
-		JLabel lblValorDaVenda = new JLabel("Valor da venda: ");
-		lblValorDaVenda.setBounds(15, 110, 106, 14);
-		panel.add(lblValorDaVenda);
-		
 		JLabel lblR = new JLabel("R$");
 		lblR.setBounds(15, 80, 16, 14);
 		panel.add(lblR);
-		
-		JLabel lblTipoDeParcelamento = new JLabel("Tipo de parcelamento:");
-		lblTipoDeParcelamento.setBounds(15, 200, 126, 14);
-		panel.add(lblTipoDeParcelamento);
 		
 		JComboBox cbFormaPagamento = new JComboBox();
 		cbFormaPagamento.setBackground(SystemColor.text);
 		cbFormaPagamento.setBounds(15, 25, 80, 20);
 		panel.add(cbFormaPagamento);
 		cbFormaPagamento.setModel(new DefaultComboBoxModel(new String[] {"Dinheiro", "Cr\u00E9dito"}));
-		cbFormaPagamento.setSelectedIndex(1);
+		cbFormaPagamento.setSelectedIndex(0);
 		
 		JLabel lblFormaDePagamento = new JLabel("Forma de pagamento:");
 		lblFormaDePagamento.setBounds(15, 8, 146, 14);
@@ -214,4 +151,5 @@ public class Principal extends JFrame {
 		lblValorCliente.setBounds(15, 60, 156, 14);
 		panel.add(lblValorCliente);
 	}
+
 }
